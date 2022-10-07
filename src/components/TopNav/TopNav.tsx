5c7462ab7a0a12ee6c14792388ad7f67c31854ad
Amy Hua <amyhua@falconx.io>
2022-10-07 15:24:11 -0700

    Top Nav Links

--- src/components/TopNav/TopNav.tsx
+++ src/components/TopNav/TopNav.tsx
@@ -71,27 +71,35 @@ const TopNav = ({ shadow, handlePrint }: { shadow?: boolean; handlePrint?: any; 
           <Disclosure.Panel className="sm:hidden">
             <div className="pt-2 pb-3 space-y-1">
               {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-              >
-                Home
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="#"
+              <Link
+                to="/"
                 className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
               >
-                About
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="#"
+                Overview
+              </Link>
+              <Link
+                to="/resume/amy-hua"
                 className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
               >
-                Work with Us
-              </Disclosure.Button>
+                Amy Hua's Resume
+              </Link>
+              {
+                handlePrint ? <>
+                  <span
+                    className="pl-3 border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pr-4 py-2 border-l-4 text-base font-medium"
+                    onClick={handlePrint}>
+                    Print
+                  </span>
+                </> : (
+                  <Disclosure.Button
+                    as="a"
+                    href="#workwithus"
+                    className="pl-3 border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pr-4 py-2 border-l-4 text-base font-medium"
+                  >
+                    Get in Touch
+                  </Disclosure.Button>
+                )
+              }
             </div>
           </Disclosure.Panel>
         </>
